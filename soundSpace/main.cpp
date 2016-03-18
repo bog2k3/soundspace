ed7b2ca814d8c2ca425637cdfd3eddb3b0d6c547
Bogdan I <bog2k3@gmail.com>
2016-03-18 18:36:17 +0200

    wip wavegen

--- soundSpace/main.cpp
+++ soundSpace/main.cpp
@@ -6,9 +6,25 @@
  */
 
 #include "../wavegen/WavegenClient.h"
+#include <iostream>
 
 int main() {
 	WavegenClient wc("test");
+	wc.connectAsync("localhost", [](OperationResult res) {
+		switch (res) {
+		case OperationResult::InProgress:
+			std::cout << "Connection to PulseAudio server in progress...\n";
+			break;
+		case OperationResult::Failed:
+			std::cout << "Connection to PulseAudio server FAILED!!!\n";
+			break;
+		case OperationResult::Success:
+			std::cout << "Connection to PulseAudio server SUCCESS!\n";
+			break;
+		default:
+			std::cout << "Connection to PulseAudio server UNKNOWN state: " << (int)res << "\n";
+		}
+	});
 	return 0;
 }
 
